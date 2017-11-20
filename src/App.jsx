abd0d58a3f1a1b6a9d82e479d3512f28318c4eac
Scott Hill <scott289@gmail.com>
2017-11-20 15:13:02 -0800

    polyfills work

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { HashRouter as Router, Route } from 'react-router-dom'
 import Header from './components/header'
+import ComicSearch from './containers/comic-search'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/css/bootstrap-theme.css'
-import ComicSearch from './containers/comic-search'
-
-// const App = () => (
-//   <div className='App'>
-//     <div className='App-header'>
-//       <img src={logo} className='App-logo shake-rotate' alt='logo' />
-//     </div>
-//     <Hello msg='Hello World' />
-//     <Router>
-//       {renderRoutes(routes)}
-//     </Router>
-//   </div>
-// )
 
 const App = ({user}) => (
   <Router>
