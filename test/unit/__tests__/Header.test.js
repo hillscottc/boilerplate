939302ff319f1375b324d4d03ecd90ee00687e47
Scott Hill <scott289@gmail.com>
2017-11-20 10:53:01 -0800

    header test

---
+++ test/unit/__tests__/Header.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import Header from 'src/components/header'
+import * as dpwLogo from 'src/assets/dpw_logo_header.gif'
+
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('<Header />', () => {
+  it('should containd the logo', () => {
+    const wrapper = shallow(<Header />)
+    expect(wrapper.containsMatchingElement(
+      <a href={'/index.asp'}><img src={dpwLogo} /></a>
+    )).toEqual(true)
+  })
+})
